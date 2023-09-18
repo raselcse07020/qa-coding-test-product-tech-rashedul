b37e121fe222d431aacdfeb46112056bdfb3b08c
rashedul.islam <rashedmbstucse07@gmail.com>
2023-09-18 10:52:27 +0600

    Added base page

--- APIAutomation/qa-coding-test-product-tech-rashedul/src/test/java/au/foxsports/com/basepage/FoxSportsBasePage.java
+++ APIAutomation/qa-coding-test-product-tech-rashedul/src/test/java/au/foxsports/com/basepage/FoxSportsBasePage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
